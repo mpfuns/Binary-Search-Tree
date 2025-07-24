90a5892e9618b424c4e9a6d35135d02e0400ec46
Monet Paul <monetpaul23@gmail.com>
2025-07-24 12:00:17 -0400

    outline

--- main.java
+++ main.java
@@ -1,3 +1,55 @@
+//
+import java.util.Scanner;
+import  java.util.*;
 public class main {
+    // Main method to run the program
+    public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+  
+  //menu  function 
+  public static void menu() {
+    string [] options = {"Create a binary search tree", "Add a node", "Delete a node", "Print nodes by InOrder", "Print nodes by PreOrder", "Print nodes by PostOrder"};
+        System.out.println("Binary Search Tree Operations:");
+        System.out.println("1. Create a binary search tree");
+        System.out.println("2. Add a node");
+        System.out.println("3. Delete a node");
+        System.out.println("4. Print nodes by InOrder");
+        System.out.println("5. Print nodes by PreOrder");
+    }
     
+    //Create a binary search tree function 
+    public static void createBinarySearchTree() {
+        // Implementation for creating a binary search tree
+    }
+
+    //Add a node function
+    public static void addNode(int data) {
+        // Implementation for adding a node to the binary search tree
+    }
+    
+    //Delete a node function
+    public static void deleteNode(int data) {
+        // Implementation for deleting a node from the binary search tree
+    }
+
+    //Print nodes by InOrder function
+    public static void printInOrder() {
+        // Implementation for printing nodes in InOrder traversal
+    }
+    
+    //Print nodes by PreOrder function
+    public static void printPreOrder() {
+        // Implementation for printing nodes in PreOrder traversal
+    }
+
+    //Print nodes by PostOrder function
+    public static void printPostOrder() {
+        // Implementation for printing nodes in PostOrder traversal
+    }
+
+
+
+
 }
+
